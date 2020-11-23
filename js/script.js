d3e8ddd324a98176871d7d5681a0e0ada3d15bb8
Davide <kalelc351k@gmail.com>
2020-11-23 13:23:16 +0100

    inseriti i dati su vue e legato gli utenti

--- js/script.js
+++ js/script.js
@@ -1,10 +1,62 @@
+// Oggi quindi si fa milestone2 di Boolzapp
 // Creiamo BoolzApp:
 // una copia della webApp di whatsApp,
 // o almeno in parte, le funzionalità come visto non saranno complete ma saranno concentrate sulla gestione delle chat e dei messaggi.
 // Ci saranno varie milestones di release del progetto, una per giorno di lavoro.
-// Per oggi/Lunedi saremo sulla milestone1
-// Questa consisterà nel creare la struttura html e il layout completi,
-// che saranno pensati su ciò che abbiam visto.
+// Per oggi saremo sulla milestone2, chat differenziate:
+// cliccando sulla chat vedo quella corrispondente;
+// con header con dati relativi a chat attiva;
+// relativa chat in elenco rimane selezionata
+var app = new Vue({
+    el: '#root',
+    data: {
+        // utente-inchat parte fissa in alto a dx
+        arrayUinChat: [
+            {
+                lFoto: 'img/avatar_4.jpg',
+                nomeU: 'Luisa....',
+                dataOra: 'Ultimo accesso oggi 20 11 2020 12:01:32....'
+            }
+        ],
+        // array chat sinistra-main
+        arrayChat: [
+            {
+                lFoto: 'img/avatar_1.jpg',
+                nomeU: 'Michele',
+                ultimoM: 'ok...',
+                dataOra: '20 11 2020 10:52:03'
+            },
+            {   
+                lFoto: 'img/avatar_2.jpg',
+                nomeU: 'Fabio',
+                ultimoM: 'ok...',
+                dataOra: '20 11 2020 10:56:00'
+            },
+            {   
+                lFoto: 'img/avatar_4.jpg',
+                nomeU: 'Luisa',
+                ultimoM: 'ok...',
+                dataOra: '20 11 2020 12:01:32'
+            }
+        ],
+        arrayMes: [
+            {}
+        ],
+
+    },
+    methods: {
+        chatClick(uno, due) {
+            this.arrayUinChat[uno].lFoto = this.arrayChat[due].lFoto;
+            this.arrayUinChat[uno].nomeU = this.arrayChat[due].nomeU;
+            this.arrayUinChat[uno].dataOra = 'Ultimo accesso: ' + this.arrayChat[due].dataOra;
+        }
+    }
+})
+
+
+
+
+
 
 
 
@@ -13,12 +65,7 @@
 
 
 // NOTE
-// ricordatevi di andare in modo ordinato e lineare;
-// scrivere codice pulito e mantenibile vi salverà dal dover impazzire successivamente;
-// ricordatevi che qui siam su layout App e non page;
-// se non vi ricordate qualcosa su ciò che abbiam visto dell’App avete sempre l’ultima parte di video di oggi (appena lo carico);
-// quindi date una bella occhiata quando avete finito e fate del refactoring ovunque possa essere utile;
-// se proprio finite e avete ancora del tempo potete iniziare a pensare a che struttura dati potrebbe aver senso fare;
-// se anche qui per caso volete proprio iniziare ad imbastire il milestone successivo, fatelo in una sotto cartella del repo;
-// la root/cartella principale del repo avrà sempre e solo il milestone assegnato quel giorno;
-// ricordate anche di riposare, al massimo i dettagli li posso sempre aggiustare strada facendo.
+// prima di scrivere qualsiasi cosa di codice fate una bella bella pre-analisi sulla struttura dei dati;
+// usate carta o penna o quello che volete, ma fin quando non avete definito i dati che vi servono e  come strutturarli, non potete far molto;
+// cercate di descrivere nel mondo reale di cosa state parlando, che “materia” state trattando e definendo;
+// poi la riportate a ciò che JS ci mette a disposizione (array, oggetti, stringhe, numeri…. etc.).
