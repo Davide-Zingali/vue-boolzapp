18f0ec59150bd37f6c54080c082cb378cc4c3f94
Davide <kalelc351k@gmail.com>
2020-11-25 18:46:45 +0100

    aggiunto filtro per lettere

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
-// Oggi quindi si fa milestone2 di Boolzapp
 // Creiamo BoolzApp:
 // una copia della webApp di whatsApp,
 // o almeno in parte, le funzionalità come visto non saranno complete ma saranno concentrate sulla gestione delle chat e dei messaggi.
 // Ci saranno varie milestones di release del progetto, una per giorno di lavoro.
-// Per oggi saremo sulla milestone2, chat differenziate:
-// cliccando sulla chat vedo quella corrispondente;
-// con header con dati relativi a chat attiva;
-// relativa chat in elenco rimane selezionata
 var app = new Vue({
     el: '#root',
     data: {
@@ -141,16 +136,6 @@ var app = new Vue({
                     }
                 );
             }, 1000);
-        },
-        filtrare(item) {
-            console.log(item);
         }
     }
 })
-
-
-// NOTE
-// prima di scrivere qualsiasi cosa di codice fate una bella bella pre-analisi sulla struttura dei dati;
-// usate carta o penna o quello che volete, ma fin quando non avete definito i dati che vi servono e  come strutturarli, non potete far molto;
-// cercate di descrivere nel mondo reale di cosa state parlando, che “materia” state trattando e definendo;
-// poi la riportate a ciò che JS ci mette a disposizione (array, oggetti, stringhe, numeri…. etc.).
